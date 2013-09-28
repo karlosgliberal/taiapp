1cad7dc3d8a900d970901ffd77fe9698d878ce58
karlos g liberal <karlosgliberal@gmail.com>
2013-09-28 12:55:55 +0200

    Añadiendo el directorio de Aitor ya que el git push no tira

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/flatline/src/taiapp/platforms/android/bin/AndroidManifest.xml : \
+/Users/flatline/src/taiapp/platforms/android/AndroidManifest.xml \
---
+++ platforms/android/bin/HelloWorld-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/flatline/src/taiapp/platforms/android/bin/HelloWorld-debug-unaligned.apk : \
+/Users/flatline/src/taiapp/platforms/android/bin/HelloWorld.ap_ \
+/Users/flatline/src/taiapp/platforms/android/bin/classes.dex \
+/Users/flatline/src/taiapp/platforms/android/libs/cordova-3.0.0.jar \
