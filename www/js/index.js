7a8bc7d5c5eeba8561984ea486908812eec5b881
karlos g liberal <karlosgliberal@gmail.com>
2013-09-28 12:03:16 +0200

    metodo para saber donde estoy

--- www/js/index.js
+++ www/js/index.js
@@ -17,25 +17,30 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
+    estoyID: null,
+
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
+
+    dondeEstoy: function(punto){
+      app.estoyID = navigator.geolocation.watchPosition(onSuccess, app.onError, { enableHighAccuracy: true });
+      function onSuccess(position) {
+        if(position.coords.accuracy < 50){
+          navigator.geolocation.clearWatch(app.estoyID);
+        }
+      };
+    },
+
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
-    // Update DOM on a Received Event
+
+    //TODO quitar mas adelante
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
